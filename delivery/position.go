9af3803c3f165fd5d7fcaadb4b3d552267d2c5ef
raisadiakbar <muhrais791@gmail.com>
2023-03-14 09:41:16 +0900

    deliv position

--- delivery/position.go
+++ delivery/position.go
@@ -107,9 +107,18 @@ func (p *positionDelivery) EditPositionHandler(c echo.Context) error {
 	ctx := c.Request().Context()
 
 	//TODO: lakukan validasi request disini
-
 	id := c.Param("id")
 	IdInt, _ := strconv.Atoi(id)
+
+	var req request.PositionRequest
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, err.Error())
+	}
+
+	if err := req.Validate(); err != nil {
+		errVal := err.(validation.Errors)
+		return helper.ResponseValidationErrorJson(c, "Error validation", errVal)
+	}
 
 	position, err := p.positionUsecase.EditPosition(ctx, IdInt, &req)
 	if err != nil {
